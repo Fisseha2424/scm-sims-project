41fa143594f4b4cd340f7f7a524cad644e289ebe
haymi <haymanotaweke9@gmail.com>
2025-12-29 12:01:52 +0300

    Add API routes with authentication middleware

--- src/backend/routes/api.php
+++ src/backend/routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\CourseController;
 
 Route::get('/test', function () {
     return response()->json(['status' => 'ok', 'message' => 'API is working']);
@@ -18,7 +19,10 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/profile', [StudentController::class, 'getProfile']);
     Route::post('/update-phone', [StudentController::class, 'updatePhone']);
     Route::get('/courses', [\App\Http\Controllers\EnrollmentController::class, 'getCourses']);
+    Route::post('/courses', [CourseController::class, 'store']);
+    Route::get('/courses/{course}', [CourseController::class, 'show']);
+    Route::put('/courses/{course}', [CourseController::class, 'update']);
+    Route::delete('/courses/{course}', [CourseController::class, 'destroy']);
     Route::get('/grades', [\App\Http\Controllers\EnrollmentController::class, 'getGrades']);
     Route::post('/logout', [AuthController::class, 'logout']); // I found I missed logout in AuthController
 });
-
